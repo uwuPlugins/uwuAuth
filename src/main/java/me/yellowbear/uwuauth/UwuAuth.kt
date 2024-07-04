bbc21c28d512fe677a612185aa33fdacd3ef31fb
MartinRostejnsky <martin.rostejnsky@email.cz>
2024-07-04 19:31:12 +0200

    switch to Kotlin

---
+++ src/main/java/me/yellowbear/uwuauth/UwuAuth.kt
@@ -0,0 +1,12 @@
+package me.yellowbear.uwuauth;
+
+import com.velocitypowered.api.plugin.Plugin
+
+@Plugin(
+        id = "uwuauth",
+        name = "uwuAuth",
+        version = "1.0-SNAPSHOT",
+        authors = ["yellowbear"]
+)
+class UwuAuth {
+}
